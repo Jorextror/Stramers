d47cc955e5876ee3a20e3191c8002aad6d590630
Jorextror <joliveda@gmail.com>
2025-01-05 21:09:16 +0100

    ADD: Vue instalado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,6 +25,9 @@
         </style>
     </head>
     <body class="antialiased">
+        <div id="app"></div>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
         <x-nav/>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             <section class="section">
